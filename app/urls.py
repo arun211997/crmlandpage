883236dc24001424a097eca234faf3003d53c5fd
arun211997 <arun21bdx@gmail.com>
2024-03-25 21:25:35 +0530

    fifth commit

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
      path('',views.land,name='land'),
+     path('dashboard',views.dashboard,name='dashboard'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def land(request):
+    return render(request,"landing.html")
+
+def dashboard(request):
     return render(request,"landpage.html")
