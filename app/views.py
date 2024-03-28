73d9fdc8dc801e950da4979530732feea45939da
arun211997 <arun21bdx@gmail.com>
2024-03-28 21:36:16 +0530

    eighth commit

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def land(request):
-    return render(request,"landing.html")
+    return render(request,"landpage.html")
 
 def dashboard(request):
-    return render(request,"landpage.html")
+    return render(request,"dashboard.html")
 
 def archiv(request):
     return render(request,"archieved.html")
